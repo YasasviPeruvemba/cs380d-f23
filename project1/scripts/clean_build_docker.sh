a8f0c914f95c3ea166ff5308e1cd038288a41336
Yasasvi Peruvemba <44848793+YasasviPeruvemba@users.noreply.github.com>
2023-09-23 15:39:46 -0500

    Initial Setup (#2)
    Co-authored-by: excalibur <excalibur@master0.cluster.local>

--- project1/scripts/clean_build_docker.sh
+++ project1/scripts/clean_build_docker.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-sudo docker image rm $(sudo docker image ls --format '{{.Repository}} {{.ID}}' | grep 'sekwonlee' | awk '{print $2}')
+sudo docker image rm $(sudo docker image ls --format '{{.Repository}} {{.ID}}' | grep 'yperuvemba' | awk '{print $2}')
 
 cd dockerfiles
 
-sudo docker build . -f base.dockerfile -t sekwonlee/kvs:base --network=host
-sudo docker push sekwonlee/kvs:base
+sudo docker build . -f base.dockerfile -t yperuvemba/cs380dproject:base --network=host
+sudo docker push yperuvemba/cs380dproject:base
 
-sudo docker build . -f client.dockerfile -t sekwonlee/kvs:client --network=host
-sudo docker push sekwonlee/kvs:client
+sudo docker build . -f client.dockerfile -t yperuvemba/cs380dproject:client --network=host
+sudo docker push yperuvemba/cs380dproject:client
 
-sudo docker build . -f frontend.dockerfile -t sekwonlee/kvs:frontend --network=host
-sudo docker push sekwonlee/kvs:frontend
+sudo docker build . -f frontend.dockerfile -t yperuvemba/cs380dproject:frontend --network=host
+sudo docker push yperuvemba/cs380dproject:frontend
 
-sudo docker build . -f server.dockerfile -t sekwonlee/kvs:server --network=host
-sudo docker push sekwonlee/kvs:server
+sudo docker build . -f server.dockerfile -t yperuvemba/cs380dproject:server --network=host
+sudo docker push yperuvemba/cs380dproject:server
 
 cd ..
